177c5b33aca4c5ec07aab0075efc46a47012a67a
Ivan Golikov <i.golikov@bars-open.ru>
2021-07-12 21:14:40 +0300

    Testing app configuration

--- keeper/__init__.py
+++ keeper/__init__.py
@@ -3,11 +3,15 @@ from flask import (
 )
 
 
-def create_app():
+def create_app(conf: dict = None):
     from .service.app import module as service_module
 
     app = Flask(__name__)
-    app.config.from_envvar('HOOK_APP_CONFIG')
+
+    if conf is None:
+        app.config.from_envvar('HOOK_APP_CONFIG')
+    else:
+        app.config.from_mapping(conf)
 
     app.register_blueprint(service_module, url_prefix='/service')
 
